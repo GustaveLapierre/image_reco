ee969e2faa16d4999f646bd06a908f9c19cb491d
GustaveLapierre <gustave.lapierre@gmail.com>
2024-05-06 21:08:21 +0200

    add red version

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ init_img = cv2.imread('/Users/gustave/Downloads/IMG_9244.png')
 x, y, w, h = 600, 1500, 1700, 1400
 init_img = init_img[y:y+h, x:x+w]
 
+blue, green, red = cv2.split(init_img)
+
+mask = (red > 1.5 * green) & (red > 1.5 * blue)
+
+mask = np.uint8(mask * 255)
+
+result = cv2.bitwise_and(init_img, init_img, mask=mask)
+
 gray = cv2.cvtColor(init_img, cv2.COLOR_BGR2GRAY)
 _, edges = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)
+
+cv2.imshow('Red Dominant Areas', result)
 cv2.imshow('edges', edges)
 
-hsv = cv2.cvtColor(init_img, cv2.COLOR_BGR2HSV)
-
-lower_red = np.array([0, 50, 50])
-upper_red = np.array([10, 255, 255])
-mask_red = cv2.inRange(hsv, lower_red, upper_red)
-
-kernel = np.ones((13, 13), np.uint8)
-mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_OPEN, kernel)
-mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_CLOSE, kernel)
-
-cv2.imshow('mask', mask_red)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
