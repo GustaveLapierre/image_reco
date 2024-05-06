5a0b1055b3db00d2eb46bb28459e18720ab863d1
GustaveLapierre <gustave.lapierre@gmail.com>
2024-05-06 21:45:54 +0200

    Stop for now

--- circle_detection.py
+++ circle_detection.py
@@ -1,27 +1,17 @@
-def find_circle_boarder(image):
-    circle_sum_x = 0
-    circle_sum_y = 0
+import cv2
+import numpy as np
 
-    circle_min_x = 0
-    circle_max_y = 0
-    circle_max_x = 0
 
-    circle_count = 0
-    for y in range(image.shape[0]):
-        for x in range(image.shape[1]):
-            if image[y,x] is not [0,0,0]:
-                circle_count += 1
+def find_circle_border(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_blurred = cv2.GaussianBlur(gray, (9, 9), 2)
 
-                circle_sum_x += x
-                circle_sum_y += y
+    circles = cv2.HoughCircles(gray_blurred,
+                               cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=50, param2=30, minRadius=0, maxRadius=0)
 
-                circle_min_x = min(circle_min_x, x)
-                circle_max_x = max(circle_max_y, y)
-
-    if circle_count == 0:
-        raise Exception()
-    else:
-        circle_center_x = circle_sum_x / circle_count
-        circle_center_y = circle_sum_y / circle_count
-        circle_radius = (circle_max_x - circle_min_x) / 2
-        return (int(circle_center_x), int(circle_center_y)), int(circle_radius)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            return (i[0], i[1], i[2])  # (center_x, center_y, radius)
+    return None
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+from circle_detection import find_circle_border
+
 init_img = cv2.imread('/Users/gustave/Downloads/IMG_9244.png')
 
 x, y, w, h = 600, 1500, 1700, 1400
@@ -12,13 +14,34 @@ mask = (red > 1.5 * green) & (red > 1.5 * blue)
 
 mask = np.uint8(mask * 255)
 
-result = cv2.bitwise_and(init_img, init_img, mask=mask)
+red_circle = cv2.bitwise_and(init_img, init_img, mask=mask)
+
+red_image = np.zeros((h, w, 3), dtype=np.uint8)
+red_image[:] = [0, 0, 50]
+
+black_image = np.zeros((h, w, 3), dtype=np.uint8)
+black_image[:] = [0, 0, 0]
+
+fully_red_image = np.zeros((h, w, 3), dtype=np.uint8)
+fully_red_image[:] = [0, 0, 200]
+
+enhanced_red = np.where(red_circle > red_image, fully_red_image, black_image)
+
+center_x, center_y, radius = find_circle_border(enhanced_red)
+#image = cv2.circle(enhanced_red, (center_x, center_y), radius, (0, 255, 0))
+
+"""circles = cv2.HoughCircles(enhanced_red, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+
+
+if circles is not None:
+    circles = np.round(circles[0, :]).astype("int")
+    for (x, y, r) in circles:
+        cv2.circle(enhanced_red, (x, y), r, (0, 255, 0), 2)
 
 gray = cv2.cvtColor(init_img, cv2.COLOR_BGR2GRAY)
 _, edges = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)
-
-cv2.imshow('Red Dominant Areas', result)
-cv2.imshow('edges', edges)
+"""
+cv2.imshow('Red Dominant Areas', enhanced_red)
 
 
 cv2.waitKey(0)
